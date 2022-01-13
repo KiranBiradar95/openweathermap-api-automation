5a49cf0df567979caf9a79b2046c86bf0d74c232
Kiran Biradar <mail2d.ravi01@gmail.com>
2022-01-13 17:01:28 +0530

    Automated the CurrentWeatherMap API

--- src/test/java/com/quickstart/testopenweathermapapi/CurrentWeatherDetails.java
+++ src/test/java/com/quickstart/testopenweathermapapi/CurrentWeatherDetails.java
@@ -1,9 +1,12 @@
 package com.quickstart.testopenweathermapapi;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.LinkedHashMap;
 
 import static io.restassured.RestAssured.given;
 
@@ -18,13 +21,18 @@ public class CurrentWeatherDetails {
     public static final String URI = "http://api.openweathermap.org";
     public static final String EndPoint = "/data/2.5/weather";
 
+    public static final long StatusOK = 200;
+    public static final long StatusNotFound = 404;
+
+    public static int ID;
+
     /*
     Test the API response is 200 for valid input (City name)
      **/
+
     @Test
     public void getCurrentWeatherByCityName() {
         RestAssured.baseURI = URI;
-
         Response res = given()
                 .queryParam("q", "Boston")
                 .queryParam("appid", APIToken)
@@ -34,10 +42,170 @@ public class CurrentWeatherDetails {
                 .extract()
                 .response();
 
-        res.prettyPrint();
+        JsonPath js = new JsonPath(res.asString());
+        Assert.assertEquals(js.get("name").toString().toLowerCase(), "boston");
 
-        Assert.assertEquals(200, res.getStatusCode());
+        Assert.assertEquals(StatusOK, res.getStatusCode());
+        LinkedHashMap<String, Object> metadata = js.get("sys");
+        Integer ID = (Integer) metadata.get("id");
+        System.out.println(ID);
 
     }
 
+
+    @Test
+    public void verifyCurrentWeatherByIncorrectCityName() {
+
+        RestAssured.baseURI = URI;
+        Response res =
+                given()
+                        .queryParam("q", "Boston123")
+
+                        .queryParam("appid", APIToken)
+
+                        .when()
+                        .get(EndPoint)
+                        .then()
+                        .extract()
+                        .response();
+
+        res.prettyPrint();
+
+        Assert.assertEquals(StatusNotFound, res.getStatusCode());
+    }
+
+    @Test
+    public void getCurrentWeatherByCityID() {
+
+        RestAssured.baseURI = URI;
+        Response res = given()
+                .queryParam("id", 2172797)
+                .queryParam("appid", APIToken)
+                .when()
+                .get(EndPoint)
+                .then()
+                .extract()
+                .response();
+
+        JsonPath js = new JsonPath(res.asString());
+
+        Assert.assertEquals(StatusOK, res.getStatusCode());
+        Assert.assertEquals("cairns", js.get("name").toString().toLowerCase());
+        System.out.println(res.prettyPrint());
+    }
+
+    @Test
+    public void getCurrentWeatherByCityNameandCityID() {
+
+        RestAssured.baseURI = URI;
+
+        Response res = given()
+                .queryParam("id", 2172797)
+                .queryParam("name", "Cairns")
+                .queryParam("appid", APIToken)
+                .when()
+                .get(EndPoint)
+                .then()
+                .extract()
+                .response();
+
+        JsonPath js = new JsonPath(res.asString());
+
+        Assert.assertEquals(StatusOK, res.getStatusCode());
+        Assert.assertEquals("cairns", js.get("name").toString().toLowerCase());
+        System.out.println(res.prettyPrint());
+    }
+
+    @Test
+    public void getCurrentWeatherByZipCodeD() {
+
+        RestAssured.baseURI = URI;
+
+        Response res = given()
+                .queryParam("zip", 94040)
+                //.queryParam("name","Cairns")
+                .queryParam("appid", APIToken)
+                .when()
+                .get(EndPoint)
+                .then()
+                .extract()
+                .response();
+
+        Assert.assertEquals(StatusOK, res.getStatusCode());
+        JsonPath js = new JsonPath(res.asString());
+        Assert.assertEquals("mountain view", js.get("name").toString().toLowerCase());
+        System.out.println(res.prettyPrint());
+    }
+
+    // Get the weather details by using city latitude and longitude
+    @Test
+    public void getCurrentWeatherByLatLon() {
+
+        RestAssured.baseURI = URI;
+
+        Response res = given()
+                .queryParam("lat", 35)
+                .queryParam("lon", 139)
+                .queryParam("appid", APIToken)
+                .when()
+                .get(EndPoint)
+                .then()
+                .extract()
+                .response();
+
+        Assert.assertEquals(StatusOK, res.getStatusCode());
+        JsonPath js = new JsonPath(res.asString());
+        Assert.assertEquals("shuzenji", js.get("name").toString().toLowerCase());
+        System.out.println(res.prettyPrint());
+    }
+
+    @Test
+    public void getCurrentWeatherByCityNameIdZipLatLon() {
+
+        RestAssured.baseURI = URI;
+        Response res = given()
+                .queryParam("lat", 37.3855)
+                .queryParam("lon", -122.088)
+                .queryParam("name", "Mountain View")
+                .queryParam("zip", 94040)
+                .queryParam("id", 5122)
+                .queryParam("appid", APIToken)
+                .when()
+                .get(EndPoint)
+                .then()
+                .extract()
+                .response();
+
+        Assert.assertEquals(StatusOK, res.getStatusCode());
+        JsonPath js = new JsonPath(res.asString());
+        Assert.assertEquals("mountain view", js.get("name").toString().toLowerCase());
+        LinkedHashMap<String, Object> metadata = js.get("sys");
+        Assert.assertEquals(2010364, metadata.get("id"));
+        System.out.println(res.prettyPrint());
+    }
+
+    /* To verify that we are getting 404 response code when passed the incorrect ID and Zip code in the request
+    Valid values are passed for City Name, Latitude and Longitude
+    */
+
+    @Test
+    public void verifyCurrentWeatherByIncorrectIdandZip() {
+
+        RestAssured.baseURI = URI;
+        Response res = given()
+                .queryParam("lat", 37.3855)
+                .queryParam("lon", -122.088)
+                .queryParam("name", "Mountain View")
+                .queryParam("zip", 9440)
+                .queryParam("id", -1)
+                .queryParam("appid", APIToken)
+                .when()
+                .get(EndPoint)
+                .then()
+                .extract()
+                .response();
+
+        //Assert.assertEquals(StatusNotFound, res.getStatusCode());
+        System.out.println(res.prettyPrint());
+    }
 }
